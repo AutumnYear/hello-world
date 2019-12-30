8e453af0c391b2353d6e0e62f5cd0c585653ab3d
sunqiunian <sunqiunian@tuhu.cn>
2019-12-30 11:03:49 +0800

    add controller and git ignore target

--- src/main/java/com/study/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/study/helloworld/controller/HelloWorldController.java
@@ -1,4 +1,16 @@
 package com.study.helloworld.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello () {
+        String test = "Hello,World!";
+        System.out.println(test);
+        return test;
+    }
+
 }
